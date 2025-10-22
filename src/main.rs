68a7ca9ac59e526f239bce26ba7043d5eccc6ad8
Nathan Brown <32497422+KingOfTac@users.noreply.github.com>
2025-10-22 07:26:46 -0600

    add templates for css token variations

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,18 @@ struct Config {
   themes: Vec<ThemeConfig>
 }
 
+#[derive(Serialize, Debug)]
+struct Palette {
+  name: String,
+  base: (String, String),
+  key: (String, String),
+  tokens: std::collections::BTreeMap<u8, (String, String)>
+}
+
+const COLOR_TOKEN_TEMPLATE: &str = "--{% if prefix %}{{ prefix }}-{% endif %}color-{{ palette_name }}-{{ tone }}";
+const COLOR_BASE_TEMPLATE: &str = "--{% if prefix %}{{ prefix }}-{% endif %}color-{{ palette_name }}";
+const COLOR_KEY_TEMPLATE: &str = "--{% if prefix %}{{ prefix }}-{% endif %}color-{{ palette_name }}-key";
+
 fn tonal_steps() -> Vec<u8> {
   vec![05, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95]
 }
@@ -47,15 +59,37 @@ fn main() -> Result<()> {
   let config_dir = config_path.parent().unwrap_or(Path::new("."));
   let config_raw = fs::read_to_string(&config_path)?;
   let config: Config = from_str(&config_raw)?;
-  let tera = Tera::new("templates/*.tera")?;
+  let mut tera = Tera::new("templates/*.tera")?;
+
+  tera.add_raw_templates([
+    ("COLOR_TOKEN", COLOR_TOKEN_TEMPLATE),
+    ("COLOR_BASE", COLOR_BASE_TEMPLATE),
+    ("COLOR_KEY", COLOR_KEY_TEMPLATE)
+  ])?;
 
   for theme in config.themes {
+    let name = theme.name;
     let is_default = theme.default.unwrap_or(false);
     let prefix = theme.prefix.unwrap_or_default();
     let description = theme.description.unwrap_or_default();
     
     for (palette_name, palette_config) in theme.palettes {
+      let base_color = BigColor::new(palette_config.base);
+      let for_variant = palette_config.variant.unwrap();
 
+      let mut palette_context = T_Context::new();
+      palette_context.insert("prefix", prefix.as_str());
+      palette_context.insert("palette_name", palette_name.as_str());
+
+      let scale = base_color.monochromatic(Some(tonal_steps().len()));
+      let key_color = closest_to_base(&base_color, &scale)?;
+
+      // let mut palette = Palette {
+      //   base: (
+      //     tera.render("COLOR_BASE", &palette_context)?.to_string(),
+      //     base_color.to_hex_string(false)
+      //   )
+      // };
     }
   }
 
